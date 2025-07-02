df69f552e3296c465f23a8acf188ae396e3fad64
nithila <nithila.annadurai@uconn.edu>
2025-07-02 17:17:31 -0400

    adds journal.db to temp path

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
+import os
 
-engine = create_engine("sqlite:///journal.db") 
+db_path = os.path.join("/tmp", "journal.db")
+engine = create_engine(f"sqlite:///{db_path}", connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
 
 def init_db():
